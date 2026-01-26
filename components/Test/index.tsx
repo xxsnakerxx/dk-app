2d412b899f7bb9a569b3641b0ee583a1e218b06b
Dmitrii Kolesnikov <xxsnakerxx@gmail.com>
2026-01-26 16:02:48 +0100

    style: update Test component styling by changing border radius and adding uppercase text transformation

--- components/Test/index.tsx
+++ components/Test/index.tsx
@@ -6,8 +6,8 @@ type Props = {
 
 export const Test = ({ title = "Test" }: Props) => {
 	return (
-		<View className="bg-blue-300 p-4 rounded-lg">
-			<Text className="font-bold text-2xl text-center">{title}</Text>
+		<View className="bg-blue-300 p-4 rounded-xl">
+			<Text className="font-bold text-2xl text-center uppercase">{title}</Text>
 		</View>
 	);
 };
